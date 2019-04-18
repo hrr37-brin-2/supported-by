8a91477ca0cbb7a695ce90aeded2f4b860410649
Brock Ross <m.brock.ross@gmail.com>
2019-04-18 15:09:01 -0600

    Update data shape and add abiliyt to accept CLI arguments for entry qty

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,14 +1,34 @@
 const faker = require('faker');
 const fs = require('fs');
-
+const loremHipsum = require('lorem-hipsum');
 
 const dataList = [];
+const entryQty = process.argv[2];
 
-for (let i = 0; i < 10000; i++) {
-  let dataEntry = {};
-  dataEntry.name = faker.name.firstName();
-  dataEntry.descriptor = faker.hacker.adjective() + faker.hacker.noun();
-  dataList.push(dataEntry);
+for (let i = 0; i <= entryQty; i++) {
+  let albumData = {};
+
+  albumData.albumID = i;
+  let commentCount = Math.floor(Math.random() * 10);
+
+  albumData.comments = [];
+
+  for (let x = 0; x < commentCount; x++) {
+    let comment = {};
+    comment.username = faker.name.findName();
+    comment.avatar = faker.image.cats();
+    comment.text = loremHipsum({
+      count: 2,
+      units: 'sentences',
+      sentenceLowerBound: 3,
+      sentenceUpperBound: 8,
+      format: 'plain'
+    })
+
+    albumData.comments.push(comment);
+  }
+
+  dataList.push(albumData);
 }
 
 
