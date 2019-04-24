3add06f6f88eb57774b85febe654bdd90429fc68
Brock Ross <m.brock.ross@gmail.com>
2019-04-24 07:46:18 -0600

    build out cassandra insert function with batch query map

--- db/indexCass.js
+++ db/indexCass.js
@@ -6,5 +6,31 @@ const client = new cassandra.Client({
 })
 
 const insertData = async (dataArr) => {
+  try {
+    const queries = dataArr.map((entry) => {
+      return {
+        query: `INSERT INTO albumdata (albumID, comments) VALUES (?, ?)`,
+        params: [entry.albumId, JSON.stringify(entry.comments)]
+      }
+    })
 
+    const response = await client.batch(queries, {prepare: true});
+
+    return response;
+  } catch(e) {
+    console.log('error in cassandra DB insert: ', e);
+  }
 }
+
+/*
+const queries = [
+  {
+    query: 'UPDATE user_profiles SET email=? WHERE key=?',
+    params: [ emailAddress, 'hendrix' ]
+  },
+  {
+    query: 'INSERT INTO user_track (key, text, date) VALUES (?, ?, ?)',
+    params: [ 'hendrix', 'Changed email', new Date() ]
+  }
+];
+*/
