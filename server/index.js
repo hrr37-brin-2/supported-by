e5bc6ee0f17a559ae0b53f2d9199b980871f3bf5
Brock Ross <m.brock.ross@gmail.com>
2019-04-24 07:16:08 -0600

    add query timers to API routes

--- server/index.js
+++ server/index.js
@@ -21,28 +21,35 @@ app.use('/', express.static(__dirname + '/../client/dist/'))
 app.use('/:id', express.static(__dirname + '/../client/dist'));
 
 app.get('/support/:id', async (req, res) => {
+  console.time('GET request took');
   const albumId = req.params.id;
   const response = await db.getEntryByID(albumId);
+  console.timeEnd('GET request took');
   res.json(response);
 });
 
 app.post('/support', async (req, res) => {
+  console.time('POST request took');
   const commentsData = JSON.stringify(req.body);
   const response = await db.addEntry(commentsData)
+  console.timeEnd('POST request took');
   res.json(response);
 })
 
 app.put('/support/:id', async (req, res) => {
+  console.time('PUT request took');
   const albumId = req.params.id;
   const commentsData = JSON.stringify(req.body);
-  //TODO: implement db access
   const response = await db.updateEntryByID(albumId, commentsData);
+  console.timeEnd('PUT request took');
   res.json(response);
 })
 
 app.delete('/support/:id', async (req, res) => {
+  console.time('DELETE request took');
   let albumId = req.params.id;
   const response = await db.deleteEntryByID(albumId);
+  console.timeEnd('DELETE request took');
   res.json(response);
 })
 
