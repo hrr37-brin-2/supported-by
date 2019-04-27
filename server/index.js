eb7a9c8e88e4509f2453d1ab3830f4055f13fcd5
Brock Ross <m.brock.ross@gmail.com>
2019-04-27 13:09:22 -0600

    remove custom middleware to support faster POST requests

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  const timestamp = new Date();
-  console.log(`serving a ${req.method} request to url ${req.url} at ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}. Body: `, req.body);
+// app.use((req, res, next) => {
+//   const timestamp = new Date();
+//   console.log(`serving a ${req.method} request to url ${req.url} at ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}. Body: `, req.body);
 
-  next();
-})
+//   next();
+// })
 
 app.use('/', express.static(__dirname + '/../client/dist/'))
 app.use('/:id', express.static(__dirname + '/../client/dist'));
